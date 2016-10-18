003d0ac984221c0918d383ec8c8fc48dd290ae84
Codie Mullins <codie.mullins@waterfield.com>
2016-10-17 19:23:47 -0500

    Add cli variable and read min track length from env

--- load_helper.rb
+++ load_helper.rb
@@ -7,11 +7,13 @@ require 'dotenv'
 
 # Setup environment
 Dotenv.load
+cli = HighLine.new
 
 VOLUMES_TO_SKIP = ENV['VOLUMES_TO_SKIP'].split(":")
 SMS_NUMBER = ENV['SMS_NUMBER']
 TEMP_DIR = ENV['TEMP_DIR']
 TARGET_DIR = ENV['TARGET_DIR']
+MIN_TRACK_LENGTH = ENV['MIN_TRACK_LENGTH'].nil? ? 60 : ENV['MIN_TRACK_LENGTH'].to_i
 
 lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
